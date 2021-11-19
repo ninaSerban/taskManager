9f3e46a6ea69885edd7a7581c721f826941e65de
Serban <niculina.serban.ext@siemens.com>
2021-11-19 14:44:45 +0200

    clean code

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ require('./db/mongoose')
 
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
-
 const app = express()
-const port = process.env.PORT
 
 app.use(express.json()) // parse body to json
 app.use(userRouter)
 app.use(taskRouter)
 
-app.listen(port, () => {
-    console.log('Server is up on port ' + port)
+app.listen(process.env.PORT, () => {
+    console.log('Server is up on port ' + process.env.PORT)
 })
 
